d434548253223d2a3a09adff2014df23c3cfd134
Ahsan <ahsangulzar009@gmail.com>
2025-07-09 03:34:00 +0500

    login with google and respose not show are solved

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
    name: { type: String, required: true },
    email: { type: String, required: true },
-   password: { type: String, required: true },
+   password: { type: String, required: false },
    promptId: [
       { type: mongoose.Schema.Types.ObjectId, ref: 'prompt' }
    ]
