fa70fe5ee01d8a17c33ec0bff2a5fdcfc6bdd288
Hassan-Islam <hassananxari4@gmail.com>
2025-09-03 15:46:15 +0500

    Fix CORS configuration for Azure deployment

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
    cors({
-      origin: 'http://localhost:5173',
+      origin: ['http://localhost:5173', 'https://financial-adviser-hassan-app.azurewebsites.net'],
       credentials: true,
    })
 );
