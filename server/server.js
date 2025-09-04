6395f15afbb68eeeb515226908ebf059fbef4c89
Hassan-Islam <hassananxari4@gmail.com>
2025-09-04 11:40:56 +0500

    Add CI/CD pipeline for local deployment

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
    cors({
-      origin: 'https://financial-adviser-hassan-app.azurewebsites.net',
+      origin: ['http://localhost:5173'],
       credentials: true,
    })
 );
